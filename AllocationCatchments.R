0583e8a69e72b60d4b2c5d8ae40e4cb15268ce7b
DaafDP <david.el.depue@gmail.com>
2017-09-05 16:48:19 +0200

    5/9/17 bis

--- AllocationCatchments.R
+++ AllocationCatchments.R
@@ -1,10 +1,33 @@
 ##Intersect municipalities & AHO water catchments (intermediary level) in QGIS
-#Using GRASS v.overlay function (ignore minor topology problems)
+#Using GRASS v.overlay function (ignoring minor topology problems)
 #Use R to obtain fractions per of water catchment per municipality
-#wide table 308 rows (municipalities) x 562 columns (water catchments)
-#subsequently converted to long format for easy input in GDX
+
+#Clear environment
+rm(list=ls())
 
 #Read in data
 SB_Gem <- read.csv("Overlay_Gem_AHO.csv")
 
-aggregate(SB_Gem, by=list(SB_Gem$b_NISCODE), FUN=sum)
+#Sum together pieces of same catchment per municipality
+CatchmentAreas <- aggregate(SB_Gem$Area, by = list(SB_Gem$a_AHO, SB_Gem$b_NISCODE), FUN=sum)
+colnames(CatchmentAreas) <- c("Catchment", "NIS", "Area")
+
+#Calculate total area 
+TotalArea <- aggregate(CatchmentAreas$Area, by=list(CatchmentAreas$NIS), FUN=sum)
+colnames(TotalArea) <- c("NIS", "MunArea")
+
+#Merge TotalArea with SB_Gem
+CatchmentAreas <- merge(CatchmentAreas, TotalArea, by="NIS")
+
+#AreaShare
+CatchmentAreas$AreaShare <- CatchmentAreas$Area /CatchmentAreas$MunArea
+
+#Add Municipality names to dataframe
+Names <- unique(SB_Gem[,c("b_NISCODE", "b_NAAM")])
+colnames(Names) <- c ("NIS", "Name")
+CatchmentAreas <- merge(Names, CatchmentAreas, by="NIS")
+
+#Write csv
+write.csv(CatchmentAreas, "CatchmentAreas_AHO.csv")
+
+
